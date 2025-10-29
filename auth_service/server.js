316a63b12f0dbc0732eacd30d202708da02d6cf8
TranDuong874 <tranthaiduong95969807@gmail.com>
2025-10-29 14:54:42 +0700

    feat: added token authentication server

---
+++ auth_service/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { useTransition } = require('react');
+const bodyParser = requrie('body-parser');
+
+const app = express()
+app.use(bodyParser.json());
+
+const SECRET = 'demo-secret-key';
+
+const users = [
+    {
+        id : 1, 
+        username : 'duong',
+        password : bcrypt.hashSync('password', 10)
+    }
+]
+
+app.post('/token', authenticate);
+const authenticate = (req, res) => {
+    const {username, password} = req.body
+
+    const user = users.find(user => user.username === username);
+    if (!user) { 
+        return res.status(401).json({error : 'Invalid username or password'});
+    }
+
+    const valid = bcrypt.compareSync(password, user.password);
+
+    if (!valid) {
+        return res.status(401).json({error : 'Invalid username or password'});
+    }
+
+    // Header is auto-generated
+    // Only define payload
+    // Signature is also auto-generated
+    // Token type: Bearer
+    // https://curity.medium.com/the-different-token-types-and-formats-explained-19dd8b947b2e
+    const token = jwt.sign(
+        {sub: user.id, username: user.username}, // Payload
+        SECRET, // Signing key
+        {expiresIn: '1h'}
+    );
+
+    // Access token: The signed JWT token, returned by the server
+    // Bearer token: The same token, sent by user
+    res.json({access_token: token, token_type: 'Bearer'});
+}
+
+PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Auth server running on http://localhost${PORT}`)
+})
